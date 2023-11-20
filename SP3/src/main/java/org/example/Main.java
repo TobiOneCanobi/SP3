d74d61d71f625603f1418ef584aba0aefb84a96f
TobiOneCanobi <vlogkongen@gmail.com>
2023-11-20 14:14:32 +0100

    Update

--- SP3/src/main/java/org/example/Main.java
+++ SP3/src/main/java/org/example/Main.java
@@ -3,5 +3,9 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
+        FileIO io = new FileIO();
+       // System.out.println(io.readUserData("users.txt"));
+        System.out.println(io.readMediaData("100bedstefilm.txt"));
+
     }
 }
