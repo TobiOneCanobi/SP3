b93e282bb3a3a769a8e6893e9bc03d201e313fd0
peterjanas <pejanas@gmail.com>
2023-11-21 10:44:09 +0100

    nye setup

--- SP3/src/main/java/org/example/Setup.java
+++ SP3/src/main/java/org/example/Setup.java
@@ -1,11 +1,30 @@
 package org.example;
-
-import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Setup {
+    FileIo io = new FileIo();
 
-    TextUI ui;
-    FileIO io;
-    ArrayList<String> allMedia;
+    public static HashMap<String,String> users = new HashMap<>();
+
+
+    public void setup() {
+        ArrayList<String> data = io.readUserData("src/User.txt");
+
+        for (String s : data) {
+            String[] row = s.split(",");
+            String userName = row[0].trim();
+            String passWord = row[1].trim();
+            registerUser(userName, passWord);
+
+        }
+        System.out.println(users); //Tester
+    }
+
+    private void registerUser(String userName, String passWord){
+        //User u = new User(userName,passWord);
+        users.put(userName,passWord);
+        //users.put(u.toString(),"");
+        //Skal der laves user objects??
+    }
 }
