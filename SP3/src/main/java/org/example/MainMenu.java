6306bf750fe10cfb86224745b35db8e6225332e1
HenrikThunbo10 <henningthunbo@hotmail.com>
2023-11-23 14:32:16 +0100

    update of searchMedia in MainMenu class

--- SP3/src/main/java/org/example/MainMenu.java
+++ SP3/src/main/java/org/example/MainMenu.java
@@ -1,24 +1,28 @@
 package org.example;
 
+import java.lang.reflect.Array;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 
-public class MainMenu  {
+public class MainMenu {
     TextUI ui = new TextUI();
     //Login login = new Login();
-    Scanner scan = new Scanner(System.in);
+    Scanner scan = new Scanner(System.in, StandardCharsets.UTF_8);
     ArrayList<String> testgenre = new ArrayList<>(); // tester
 
-    Movie it = new Movie("it",1999,testgenre,9.1); // tester
+    Movie it = new Movie("it", 1999, testgenre, 9.1); // tester
     HashMap<User, ArrayList<Objects>> hashMapWatchedMedia = new HashMap<>();
     Setup setup = new Setup();
 
-    public void welcome(User currentUser){
+    FileIO io = new FileIO();
+
+    public void welcome(User currentUser) {
         testgenre.add("action");
-    ui.displayMessage("Welcome " + currentUser.getUsername() +  " These are your options, type the number assigned to the option" +
-            "\n1: Search Media.\n2: Search Genre.\n3: Check your viewed movies & series." +
-            "\n4: Check your saved movie & series.\n5: Save and exit.");
+        ui.displayMessage("Welcome " + currentUser.getUsername() + " These are your options, type the number assigned to the option" +
+                "\n1: Search Media.\n2: Search Genre.\n3: Check your viewed movies & series." +
+                "\n4: Check your saved movie & series.\n5: Save and exit.");
         String input = scan.nextLine();
-        switch(input) {
+        switch (input) {
             case "1":
                 searchMedia(currentUser);
                 break;
@@ -41,27 +45,75 @@ public class MainMenu  {
         }
     }
 
-    public void searchMedia(User currentUser){
-        System.out.println("search media now");
+    public void searchMedia(User currentUser) {
+
+        /*
+        ArrayList<String> movieData = io.readMediaData("Textdata/100bedstefilm.txt");
+        ArrayList<String> seriesData = io.readMediaData("Textdata/100bedsteserier.txt");
+
+        ArrayList<String> movieTitle = new ArrayList<>();
+        ArrayList<String> seriesTitle = new ArrayList<>();
+
+        for (String s : movieData) {
+            String[] row = s.split(";");
+            movieTitle.add(row[0].trim());
+        }
+
+        for (String s : seriesData) {
+            String[] row = s.split(";");
+            seriesTitle.add(row[0].trim());
+        }
+
+        movieTitle.addAll(seriesTitle);
+        System.out.println(movieTitle);
+        System.out.println("Please search for a movie or series.");
+        String searchForMedia = scan.nextLine();
+
+        if (movieTitle.contains(searchForMedia)) {
+            System.out.println(searchForMedia + " is now playing." + "\n" + "Thanks for watching " + searchForMedia + ".");
+
+        } else {
+            System.out.println("Media not found. Please search again or browse movie genres.");
+            searchMedia(currentUser);
+
+        }
+        */
+
+
+        setup.getMovies();
+        setup.getSeries();
+
+        System.out.println(setup.getMovies());
+        System.out.println("Please search for a movie or series.");
+        String searchForMedia = scan.nextLine();
+
+        if (setup.getMovies().contains(searchForMedia)) {
+            System.out.println(searchForMedia + " now playing." + "\n" + "Thanks for watching " + searchForMedia + ".");
+        } else {
+            System.out.println("The media you searched for wasn't found. Please search again or browse movie genres.");
+            searchMedia(currentUser);
+
+        }
 
     }
 
-    public void searchGenre(User currentUser){
+    public void searchGenre(User currentUser) {
         System.out.println("search genre now");
         //setup.getMovies(); tester
 
     }
 
-    public void checkViewedMedia(User currentUser){
+    public void checkViewedMedia(User currentUser) {
         System.out.println("here is your viewed media");
         currentUser.addToWatchedMedia(it);
         System.out.println(currentUser.getWatchedMedia());
     }
 
-    public void checkSavedMedia(User currentUser){
+    public void checkSavedMedia(User currentUser) {
         System.out.println("here is your saved media");
     }
-    public void saveAndExit(User currentUser){
+
+    public void saveAndExit(User currentUser) {
         System.out.println("saving and exiting");
     }
 
